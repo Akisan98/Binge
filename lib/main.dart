82fd3510f9ea1fb2c492fbee25ddba2092d457d7
Akisan <akisan.98@gmail.com>
2022-04-02 13:27:07 +0200

    FEAT: StartPage

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,194 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  // await dotenv.load(fileName: ".env");
+  runApp(const Binge());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class Binge extends StatelessWidget {
+  const Binge({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  final Color purple = const Color.fromARGB(255, 88, 14, 247);
+  final Color white = const Color.fromARGB(255, 255, 255, 255);
+  final Color black = const Color.fromARGB(255, 0, 0, 0);
+
+  final Color dark = const Color.fromARGB(255, 27, 28, 33);
+  final Color light = const Color.fromARGB(255, 238, 204, 215);
+
+  final TextStyle button = const TextStyle(
+    color: Colors.white,
+    fontSize: 16,
+    fontWeight: FontWeight.w600,
+  );
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        // Light Theme
+        brightness: Brightness.light,
+        backgroundColor: light,
+        primaryColor: purple,
+        splashColor: Colors.black,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: purple,
+          secondary: dark,
+          tertiary: white,
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            primary: purple,
+            minimumSize: const Size.fromHeight(56),
+            textStyle: button,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8),
+            ),
+          ),
+        ),
+        textTheme: TextTheme(
+          button: button,
+          overline: TextStyle(
+            color: white,
+            fontSize: 16,
+            fontWeight: FontWeight.w600,
+            letterSpacing: 0.5,
+          ), // button 2
+          headline1: TextStyle(
+            color: black,
+            fontSize: 48,
+            fontWeight: FontWeight.w900,
+          ),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      darkTheme: ThemeData(
+        // Dark Theme
+        backgroundColor: dark,
+        brightness: Brightness.dark,
+
+        primaryColor: purple,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: purple,
+          secondary: dark,
+          tertiary: white,
+          brightness: Brightness.dark,
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            primary: purple,
+            minimumSize: const Size.fromHeight(56),
+            textStyle: button,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8),
+            ),
+          ),
+        ),
+        textTheme: TextTheme(
+          button: button,
+          overline: TextStyle(
+            color: purple,
+            fontSize: 16,
+            fontWeight: FontWeight.w600,
+            letterSpacing: 0.5,
+          ), // button 2
+          headline1: TextStyle(
+            color: white,
+            fontSize: 48,
+            fontWeight: FontWeight.w900,
+          ),
+        ),
+      ),
+      themeMode: ThemeMode.system,
+      debugShowCheckedModeBanner: true,
+      home: const LoginPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    return const Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Text("Hello World"),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var theme = Theme.of(context);
+
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(32, 135, 32, 60),
+            child: Column(
+              children: [
+                Text(
+                  "Welcome 👋",
+                  style: theme.textTheme.headline1,
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(
+                    top: 32,
+                    left: 16,
+                    right: 16,
+                  ),
+                  child: Text(
+                    "Binge is the app to keep track of all of your TV Shows and movies and how far you have progressed.",
+                    textScaleFactor: 1.1,
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                const Spacer(),
+                ElevatedButton(
+                  onPressed: (() {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(
+                        content: Text("Privacy is a good thing!"),
+                      ),
+                    );
+                  }),
+                  child: Text(
+                    "Let's Start!",
+                    style: theme.textTheme.button,
+                  ),
+                ),
+                // const SizedBox(
+                //   height: 18,
+                // ),
+                // ElevatedButton(
+                //   onPressed: (() {
+                //     ScaffoldMessenger.of(context).showSnackBar(
+                //       const SnackBar(
+                //         content: Text("No Support for web"),
+                //       ),
+                //     );
+                //   }),
+                //   child: Text(
+                //     "Sign in with Trakt",
+                //     style: theme.textTheme.overline,
+                //   ),
+                //   style: ElevatedButton.styleFrom(
+                //     primary: theme.brightness == Brightness.light
+                //         ? theme.colorScheme.secondary
+                //         : theme.colorScheme.tertiary,
+                //   ),
+                // ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
